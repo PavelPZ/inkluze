84f19c59ecefda4bcba32b6173a1e7260f448a6a
PZ <pzika@langmaster.cz>
2016-08-25 11:14:00 +0200

    v

--- inkluze/apps/inkluze/pages/popups.tsx
+++ inkluze/apps/inkluze/pages/popups.tsx
@@ -4,13 +4,19 @@
       Jakekoliv HTML s tagy
     </div>
   </Popup>,
+  yyy: <Popup document='priloha3' page= {124} title='yyy'>
+    <div>
+      Jakekoliv HTML s tagy
+    </div>
+  </Popup>,
 }
 
 const DumpPopups: React.StatelessComponent<{}> = prop => {
   var pops: Array<JSX.Element> = [];
   for (var p in popups) {
     pops.push(popups[p]);
+    pops.push(<hr/>);
   }
-  return <div>{pops}<hr/></div>
+  return <div>{pops}</div>
 }
 
