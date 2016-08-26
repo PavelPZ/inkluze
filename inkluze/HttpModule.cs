e16aefb4b3456816953525ad94010f6ad71759f3
PZ <pzika@langmaster.cz>
2016-08-26 15:55:12 +0200

    Home OK

--- inkluze/HttpModule.cs
+++ inkluze/HttpModule.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.IO;
-using System.Web;
+﻿//using System;
+//using System.IO;
+//using System.Web;
 
-namespace inkluze {
-    public class HttpModule : IHttpModule {
-        public void Dispose() { }
+//namespace inkluze {
+//    public class HttpModule : IHttpModule {
+//        public void Dispose() { }
 
-        public void Init(HttpApplication context) {
-            context.BeginRequest += (object sender, EventArgs e) => {
-                var app = sender as HttpApplication; if (app == null) return;
-                var url = app.Request.Url.AbsolutePath; if (!url.ToLower().StartsWith("/index.html")) return;
-                var fileName = app.Server.MapPath("~/index.html");
-                app.Response.Write(File.ReadAllText(fileName));
-                app.Response.End();
-            };
-        }
-    }
-}
+//        public void Init(HttpApplication context) {
+//            context.BeginRequest += (object sender, EventArgs e) => {
+//                var app = sender as HttpApplication; if (app == null) return;
+//                var url = app.Request.Url.AbsolutePath; if (!url.ToLower().StartsWith("/index.html")) return;
+//                var fileName = app.Server.MapPath("~/index.html");
+//                app.Response.Write(File.ReadAllText(fileName));
+//                app.Response.End();
+//            };
+//        }
+//    }
+//}
