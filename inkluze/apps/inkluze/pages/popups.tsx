b5b5f503856c792a741a35b51f9482a86f2ebca5
Petr Janeček <pjanecek@langmaster.cz>
2016-08-26 16:01:14 +0200

    oprava teček

--- inkluze/apps/inkluze/pages/popups.tsx
+++ inkluze/apps/inkluze/pages/popups.tsx
@@ -2,22 +2,22 @@
   MAP: <Popup document='priloha3' page={3} title='Jak identifikovat oblasti pro rozvoj školy?'>
     <div>
       <p>
-        V listopadu roku 2015 byl školám v rámci šetření realizovaného pro potřeby výzvy Místní akční plány rozvoje vzdělávání (MAP) rozeslán dotazník, jehož vyplnění je podmínkou možnosti čerpání finančních prostředků prostřednictvím projektů zjednodušeného vykazování (šablon).Při zpracovávání dotazníků si školy ujasnily, ve kterých oblastech by se měly a chtěly rozvíjet.Školám pomohou v identifikaci jejich potřeb výsledky a vyhodnocení Výstupu z dotazníkového šetření MAP, které budou školám zaslány ze strany MŠMT a zpřístupněny ve webovém rozhraní https://sberdat.uiv.cz/login. Součástí tohoto vyhodnocení je také seznam šablon, ze kterých mohou školy volit, aby alespoň část jejich potřeb rozvoje byla naplněna. Výstup z dotazníkového šetření MAP2 bude povinnou přílohou Žádosti o podporu.
+        V listopadu roku 2015 byl školám v rámci šetření realizovaného pro potřeby výzvy Místní akční plány rozvoje vzdělávání (MAP) rozeslán dotazník, jehož vyplnění je podmínkou možnosti čerpání finančních prostředků prostřednictvím projektů zjednodušeného vykazování (šablon). Při zpracovávání dotazníků si školy ujasnily, ve kterých oblastech by se měly a chtěly rozvíjet. Školám pomohou v identifikaci jejich potřeb výsledky a vyhodnocení Výstupu z dotazníkového šetření MAP, které budou školám zaslány ze strany MŠMT a zpřístupněny ve webovém rozhraní https://sberdat.uiv.cz/login. Součástí tohoto vyhodnocení je také seznam šablon, ze kterých mohou školy volit, aby alespoň část jejich potřeb rozvoje byla naplněna. Výstup z dotazníkového šetření MAP2 bude povinnou přílohou Žádosti o podporu.
       </p>
       <div className="well well-sm">
-        Škola si povinně volí minimálně jednu šablonu z oblasti, která bude vyhodnocena jako nejslabší vzhledem k celorepublikovému průměru.Vzhledem k tomu, že dotazník byl koncipován šířeji pro programové období, je možné, že škola nenalezne odpovídající šablonu (např.oblast digitálních technologií v případě mateřské školy), pak bude volit ze šablon zaměřených na druhou nejslaběji vyhodnocenou oblast, pro kterou jsou k dispozici šablony.Volba minimálně jedné šablony dle uvedeného bude předmětem kontroly přijatelnosti projektu.Pokud škola tuto šablonu nezvolí, bude žádost vyřazena.
+        Škola si povinně volí minimálně jednu šablonu z oblasti, která bude vyhodnocena jako nejslabší vzhledem k celorepublikovému průměru. Vzhledem k tomu, že dotazník byl koncipován šířeji pro programové období, je možné, že škola nenalezne odpovídající šablonu (např. oblast digitálních technologií v případě mateřské školy), pak bude volit ze šablon zaměřených na druhou nejslaběji vyhodnocenou oblast, pro kterou jsou k dispozici šablony. Volba minimálně jedné šablony dle uvedeného bude předmětem kontroly přijatelnosti projektu. Pokud škola tuto šablonu nezvolí, bude žádost vyřazena.
       </div>
       <p>
-        Příklad: Mateřská škola má nejslabší hodnocení v oblasti digitálních technologií.Pro tuto oblast nejsou v 1. vlně šablon připraveny žádné šablony.Jako druhou nejslabší má vyhodnocenou oblast matematické pregramotnosti.Bude tedy volit alespoň jednu šablonu zaměřenou na podporu matematické pregramotnosti.
+        Příklad: Mateřská škola má nejslabší hodnocení v oblasti digitálních technologií. Pro tuto oblast nejsou v 1. vlně šablon připraveny žádné šablony. Jako druhou nejslabší má vyhodnocenou oblast matematické pregramotnosti. Bude tedy volit alespoň jednu šablonu zaměřenou na podporu matematické pregramotnosti.
       </p>
       <p>
-        Po splnění této podmínky může škola volit další šablony podle svého rozhodnutí do výše maximální částky na projekt.Výběr šablon musí být uvážlivý.Každá škola by měla před sestavením projektu nejen vyhodnotit oblasti z Výstupu dotazníkového šetření MAP, ve kterých se chce/potřebuje zlepšovat, ale také např.zhodnotit své časové a administrativní kapacity, případně také nabídku dalšího vzdělávání pedagogických pracovníků (dále jen „DVPP“) nebo dostatečných kapacit pro obsazení pozic personální podpory mateřský škol (MŠ) a základních škol (ZŠ).Pro rozhodování je také klíčové omezení výzvy v podobě minimální a maximální výše rozpočtu.
+        Po splnění této podmínky může škola volit další šablony podle svého rozhodnutí do výše maximální částky na projekt. Výběr šablon musí být uvážlivý. Každá škola by měla před sestavením projektu nejen vyhodnotit oblasti z Výstupu dotazníkového šetření MAP, ve kterých se chce/potřebuje zlepšovat, ale také např. zhodnotit své časové a administrativní kapacity, případně také nabídku dalšího vzdělávání pedagogických pracovníků (dále jen „DVPP“) nebo dostatečných kapacit pro obsazení pozic personální podpory mateřský škol (MŠ) a základních škol (ZŠ). Pro rozhodování je také klíčové omezení výzvy v podobě minimální a maximální výše rozpočtu.
       </p>
       <p>
-        Na závěr projektu školy vyplní dotazník znovu, aby zjistily, k jakému posunu došlo.Tento posun bude součástí doložení indikátoru Počet organizací, ve kterých se zvýšila kvalita výchovy a vzdělávání a proinkluzivnost.Odkaz na dotazník k vyplnění v závěru projektu bude zveřejněn na webových stránkách Ministerstva školství, mládeže a tělovýchovy (dále jen „MŠMT“).Školy vyplní dotazník před koncem projektu v době, kdy budou mít co nejvíce aktivit (šablon) realizovaných.Výstup s vyhodnocením znovu vyplněného dotazníku bude přílohou závěrečné zprávy o realizaci (ZZoR).
+        Na závěr projektu školy vyplní dotazník znovu, aby zjistily, k jakému posunu došlo. Tento posun bude součástí doložení indikátoru Počet organizací, ve kterých se zvýšila kvalita výchovy a vzdělávání a proinkluzivnost. Odkaz na dotazník k vyplnění v závěru projektu bude zveřejněn na webových stránkách Ministerstva školství, mládeže a tělovýchovy (dále jen „MŠMT“). Školy vyplní dotazník před koncem projektu v době, kdy budou mít co nejvíce aktivit (šablon) realizovaných. Výstup s vyhodnocením znovu vyplněného dotazníku bude přílohou závěrečné zprávy o realizaci (ZZoR).
       </p>
       <p>
-        Pokud škola výstup z dotazníku nedoloží, nebude moci vykázat indikátor výsledku.Při nedoložení indikátoru výsledku budou ze strany poskytovatele dotace uplatněny sankce dle kap.11.3.Pravidel pro žadatele a příjemce zjednodušených projektů.
+        Pokud škola výstup z dotazníku nedoloží, nebude moci vykázat indikátor výsledku. Při nedoložení indikátoru výsledku budou ze strany poskytovatele dotace uplatněny sankce dle kap.11.3.Pravidel pro žadatele a příjemce zjednodušených projektů.
       </p>
     </div>
   </Popup>,
@@ -25,11 +25,11 @@
   zadavatel: <Popup document='vyzva' page= {5} title='Oprávnění žadatelé'>
     <div>
       <p>
-        Mateřské  a  základní  školy  nezřizované  organizačními  složkami  státu (tj.právnická osoba vykonávající činnost školy, zapsaná ve školském
+        Mateřské  a  základní  školy  nezřizované  organizačními  složkami  státu (tj. právnická osoba vykonávající činnost školy, zapsaná ve školském
         rejstříku), které  mají  veškolním roce, v němž  žádost  o  podporu podávají, minimálně jedno dítě/žáka.
       </p>
       <p>
-        Mateřské a základní školy zřizované Ministerstvem školství, mládeže a tělovýchovy dle školského zákona (tj.právnická osoba vykonávající
+        Mateřské a základní školy zřizované Ministerstvem školství, mládeže a tělovýchovy dle školského zákona (tj. právnická osoba vykonávající
         činnost  školy, zapsaná  ve  školském  rejstříku), které  mají ve školním roce, v němž žádost o podporu podávají, minimálně jedno
         dítě/žáka.
       </p>
@@ -46,46 +46,46 @@
           Minimální výše: 200 000 Kč
         </p>
         <p>
-          Maximální výše: maximální výše finanční podpory na jeden projekt se stanoví dle tohoto vzorce: 200 000 Kč + (počet dětí/žáků školy x 2 200 Kč) = maximální částka na školu.V případě, že součástí právnické osoby je mateřská a základní škola, se částka 200 000 Kč počít á jeden krát za mateřskou školu a jeden krát za základní školu, celkem tedy 400 000 Kč.
+          Maximální výše: maximální výše finanční podpory na jeden projekt se stanoví dle tohoto vzorce: 200 000 Kč + (počet dětí/žáků školy x 2 200 Kč) = maximální částka na školu. V případě, že součástí právnické osoby je mateřská a základní škola, se částka 200 000 Kč počít á jeden krát za mateřskou školu a jeden krát za základní školu, celkem tedy 400 000 Kč.
         </p>
         <p>
-          Počet dětí/ žáků školy k 30.9.2015 bude zveřejněn u vyhlášené výzvy na webových stránkách MŠMT.Počet dětí/žáků školy pro další školní roky bude vždy aktualizován v prosinci daného školního roku na webových stránkách MŠMT.
+          Počet dětí/ žáků školy k 30.9.2015 bude zveřejněn u vyhlášené výzvy na webových stránkách MŠMT. Počet dětí/žáků školy pro další školní roky bude vždy aktualizován v prosinci daného školního roku na webových stránkách MŠMT.
         </p>
         <p>
           Maximální výši podpory na jeden projekt žadatel vypočítá v „Kalkulačce indikátorů“, která je povinnou přílohou Žádosti o podporu.
         </p>
       </div>
       <p>
-        <b>Příklad č.1: </b> Škola má ve školním roce, ve kterém podává žádost, 100 dětí/žáků.Škola může čerpat maximálně 200 000 + (100 x 2 200 Kč) = 420 000 Kč.
+        <b>Příklad č.1: </b> Škola má ve školním roce, ve kterém podává žádost, 100 dětí/žáků. Škola může čerpat maximálně 200 000 + (100 x 2 200 Kč) = 420 000 Kč.
       </p>
       <p>
-        <b>Příklad č.2: </b> Škola má celkem pět odloučených pracovišť.Ve školním roce, ve kterém podává žádost, má celkem 320 dětí/žáků.Škola může čerpat maximálně 200 000 + (320 x 2 200 Kč) = 904 000 Kč.
+        <b>Příklad č.2: </b> Škola má celkem pět odloučených pracovišť. Ve školním roce, ve kterém podává žádost, má celkem 320 dětí/žáků. Škola může čerpat maximálně 200 000 + (320 x 2 200 Kč) = 904 000 Kč.
       </p>
       <p>
-        <b>Příklad č.3: </b> Součástí právnické osoby je základní škola se 120 žáky a mateřská škola s 28 dětmi.Škola jako celá právnická osoba může čerpat maximálně 200 000 + 200 000 + (148 x 2 200 Kč) = 725 600 Kč.
+        <b>Příklad č.3: </b> Součástí právnické osoby je základní škola se 120 žáky a mateřská škola s 28 dětmi. Škola jako celá právnická osoba může čerpat maximálně 200 000 + 200 000 + (148 x 2 200 Kč) = 725 600 Kč.
       </p>
       <p>
-        V tomto případě je třeba, aby ředitel školy volil šablony jak pro mateřskou školu, tak pro základní školu, a to poměrně podle počtu dětí/žáků.Tzn., z celkové maximální částky na projekt budou na základní školu využity prostředky v max.výši 200 000 + (120 x 2 200 Kč) = 464 000 Kč, pro mateřskou školu prostředky v max.výši: 200 000 + (28 x 2 200 Kč) = 261 600 Kč.
+        V tomto případě je třeba, aby ředitel školy volil šablony jak pro mateřskou školu, tak pro základní školu, a to poměrně podle počtu dětí/žáků. Tzn., z celkové maximální částky na projekt budou na základní školu využity prostředky v max.výši 200 000 + (120 x 2 200 Kč) = 464 000 Kč, pro mateřskou školu prostředky v max.výši: 200 000 + (28 x 2 200 Kč) = 261 600 Kč.
       </p>
     </div>
   </Popup>,
 
   ms_skolni_asistent: <Popup document='priloha3' page={18} title='I/1.1 Školní asistent – personální podpora MŠ'>
     <div>
-      <p>Školní asistent poskytuje základní nepedagogickou podporu přímo v rodině při spolupráci s rodiči, zprostředkovává komunikaci s komunitou, rodinou a školou spočívající např.v aktivitách vedoucích k zajištění pravidelné docházky dětí, porozumění rodinnému prostředí dětí a zajištění přenosu informací mezi mateřskou školou a rodinou, poskytuje přímou nepedagogickou podporu dětí v předškolním vzdělávání spočívající např.v nácviku jednoduchých činností při příchodu a pobytu ve škole nebo školském zařízení nebo při akcích školy nebo školského zařízení, pomoci při oblékání, poskytuje podporu pedagogovi při administrativní a organizační činnosti pedagoga ve vyučování i mimo vyučování.</p>
+      <p>Školní asistent poskytuje základní nepedagogickou podporu přímo v rodině při spolupráci s rodiči, zprostředkovává komunikaci s komunitou, rodinou a školou spočívající např. v aktivitách vedoucích k zajištění pravidelné docházky dětí, porozumění rodinnému prostředí dětí a zajištění přenosu informací mezi mateřskou školou a rodinou, poskytuje přímou nepedagogickou podporu dětí v předškolním vzdělávání spočívající např. v nácviku jednoduchých činností při příchodu a pobytu ve škole nebo školském zařízení nebo při akcích školy nebo školského zařízení, pomoci při oblékání, poskytuje podporu pedagogovi při administrativní a organizační činnosti pedagoga ve vyučování i mimo vyučování.</p>
     </div>
   </Popup>,
 
   ms_specialni_pedagog: <Popup document= 'priloha3' page= {23} title= 'I/1.2 Školní speciální pedagog – personální podpora MŠ' >
     <div>
-      <p>Speciální pedagog diagnostikuje speciální vzdělávací potřeby dětí a pomáhá vytvářet a zlepšit podmínky pro úspěšnou integraci dětí se speciálními vzdělávacími potřebami.Součástí práce je spolupráce na tvorbě plánu pedagogické podpory nebo individuálního vzdělávacího plánu pro každé dítě s potřebou podpůrných opatření/se speciálními vzdělávacími potřebami.</p>
+      <p>Speciální pedagog diagnostikuje speciální vzdělávací potřeby dětí a pomáhá vytvářet a zlepšit podmínky pro úspěšnou integraci dětí se speciálními vzdělávacími potřebami. Součástí práce je spolupráce na tvorbě plánu pedagogické podpory nebo individuálního vzdělávacího plánu pro každé dítě s potřebou podpůrných opatření/se speciálními vzdělávacími potřebami.</p>
     </div>
   </Popup >,
 
   ms_psycholog: <Popup document='priloha3' page= {26} title='I/1.3 Školní psycholog – personální podpora MŠ'>
     <div>
       <p>
-        Školní psycholog zkoumá klima ve třídách, chování dětí, vytváří diagnostiku a poskytuje konzultace pro pedagogy a rodiče ve škole nebo mimo školu.Školní psycholog spolupracuje také se zdravotnickými a jinými organizacemi mimo školní zařízení.
+        Školní psycholog zkoumá klima ve třídách, chování dětí, vytváří diagnostiku a poskytuje konzultace pro pedagogy a rodiče ve škole nebo mimo školu. Školní psycholog spolupracuje také se zdravotnickými a jinými organizacemi mimo školní zařízení.
       </p>
     </div>
   </Popup>,
@@ -93,10 +93,10 @@
   ms_socialni_pedagog: <Popup document='priloha3' page= {29} title='I/1.4 Sociální pedagog – personální podpora MŠ'>
     <div>
       <p>
-        Sociální pedagog není pedagogický pracovník, jeho náplní práce je vytvářet propojení mezi školou a jinými subjekty, např.obcí, policií, státním zástupcem a zdravotnickým zařízením.Součástí práce je poskytování mediace mezi školou, rodiči a uvedenými institucemi a pomoc s právními a sociálními otázkami.
+        Sociální pedagog není pedagogický pracovník, jeho náplní práce je vytvářet propojení mezi školou a jinými subjekty, např. obcí, policií, státním zástupcem a zdravotnickým zařízením. Součástí práce je poskytování mediace mezi školou, rodiči a uvedenými institucemi a pomoc s právními a sociálními otázkami.
       </p>
       <p>
-        Sociální pedagog bude působit jako prostředník mezi mateřskou školou a rodinou.Účinně pomůže dětem, jejichž rodiny nemají dostatečnou kapacitu pomoci dětem při vzdělávání v mateřských školách.Poskytne pedagogům informace týkající se zázemí dětí a problémů, což následně pedagogům pomůže zvolit vhodný přístup k dítěti.
+        Sociální pedagog bude působit jako prostředník mezi mateřskou školou a rodinou. Účinně pomůže dětem, jejichž rodiny nemají dostatečnou kapacitu pomoci dětem při vzdělávání v mateřských školách. Poskytne pedagogům informace týkající se zázemí dětí a problémů, což následně pedagogům pomůže zvolit vhodný přístup k dítěti.
       </p>
     </div>
   </Popup>,
