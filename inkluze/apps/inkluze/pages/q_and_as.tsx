9a1aa5846db5c6b42a39bd101179e21f9f1a1284
PZ <pzika@langmaster.cz>
2016-08-29 16:22:38 +0200

    q&a

--- inkluze/apps/inkluze/pages/q_and_as.tsx
+++ inkluze/apps/inkluze/pages/q_and_as.tsx
@@ -1,10 +1,13 @@
 ﻿const qas = [
-  createQA(<span><i>Formated</i> question</span>,
+  createQA(
+    <span><i>Formated</i> question</span>,
     <div>
       <b>Formated answer</b><br/>
       answer
     </div>),
-  createQA('Text Question 2','Text Answer 2'),
+  createQA(
+    'Text Question 2',
+    'Text Answer 2'),
 ]
 
 
