64014d7d1426da2a7f6754c2cc10b3055ac97ae5
PZ <pzika@langmaster.cz>
2016-08-31 09:49:41 +0200

    v

--- inkluze/apps/inkluze/code/controls.tsx
+++ inkluze/apps/inkluze/code/controls.tsx
@@ -1,24 +1,25 @@
-﻿interface IBlockProps extends lib.IComponentProps { gray?: boolean; blue?: boolean; darkGray?:boolean; }
+﻿interface IBlockProps extends lib.IComponentProps { gray?: boolean; blue?: boolean; darkGray?: boolean; }
 const Block: React.StatelessComponent<IBlockProps> = (props: IBlockProps) => {
   var st: React.CSSProperties = props.gray ? { backgroundColor: '#eeeeee' } : {};
   var cls = props.blue ? 'blue-header' : (props.darkGray ? 'dark-header' : null);
-  return <div style={Object.assign(st, props.style)} className={cls}>
-    <div className="container">
+  return <div style={Object.assign(st, props.style) } className={cls}>
+    <div className="container" style={{padding: '10px'}}>
       {props.children}
     </div>
-    <br/>
   </div>;
 };
 
-interface IBlockExProps extends IBlockProps { title:string; }
+interface IBlockExProps extends IBlockProps { title: string; }
 const BlockEx: React.StatelessComponent<IBlockExProps> = (props: IBlockExProps) => {
   return React.createElement(Block, props,
-    [<h2>{props.title}</h2>,
-    <Row>
-      <Col mdOffset={2} md={8}>
-        {props.children}
-      </Col>
-    </Row>]);
+    [
+      <h2 key={1}>{props.title}</h2>,
+      <Row key={2}>
+        <Col mdOffset={2} md={8}>
+          {props.children}
+        </Col>
+      </Row>
+    ]);
 };
 
 interface IPopupProps extends lib.IComponentProps {
@@ -44,7 +45,7 @@ class Popup extends React.Component<IPopupProps, IPopupState> {
   render(): JSX.Element {
     return <span>
       <Label bsStyle="warning" onClick={() => this.open() } style={{ color: 'white', cursor: 'pointer', padding: '3px', fontSize: '100%', fontWeight: 'normal' }}>{this.props.title}</Label>
-      <Modal show={this.state.showModal} onHide={() => this.close()}>
+      <Modal show={this.state.showModal} onHide={() => this.close() }>
         <Modal.Header closeButton>
           <Modal.Title>{this.props.title}</Modal.Title>
           <p><i>{this.props.document}, stránka {this.props.page}</i></p>
