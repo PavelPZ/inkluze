b7be4af8f0e0e9c3e0dee1565296b93f4d3c06cd
Petr Janeček <pjanecek@langmaster.cz>
2016-09-05 15:56:15 +0200

    Korektury od ZZ a RJ

--- inkluze/apps/inkluze/pages/home.tsx
+++ inkluze/apps/inkluze/pages/home.tsx
@@ -47,13 +47,13 @@
     </div>
     <BlockEx header="O Inkluzi">
       <p>
-        Problematika inkluze, neboli začleňování děti se speciálními vzdělávacími potřebami do základních a mateřských škol, není žádnou letošní novinkou.
-        Novinkou je, že se inkluze konečně začíná efektivně řešit.
-        Ať už právními úpravami (nová vyhláška školského zákona) nebo velkými finančními dotacemi (které by měly školám významně pomoci inkluzi zvládnout).
+        Problematika inkluze, neboli začleňování dětí se speciálními vzdělávacími potřebami do základních a mateřských škol, není žádnou letošní novinkou.
+        Novinkou však je, že se inkluze konečně začíná efektivně řešit.
+        Ať už právními úpravami (nová vyhláška školského zákona) nebo velkými finančními dotacemi, které by měly školám významně pomoci inkluzi zvládnout.
       </p>
       <Alert type="Poznámka">
-        <b>Inkluze</b> se nově nazývá <b>Společné vzdělávání</b>.
-        My budeme ale používat doposud rozšířenější termín Inkluze.
+        <b>Inkluze</b> se nově nazývá <b>společné vzdělávání</b>.
+        My budeme ale používat doposud rozšířenější termín inkluze.
         {/*Viz též {sitemapRouter.navigateTag(inkluze.root.slovnicek) }.*/}
       </Alert>
     </BlockEx>
@@ -87,8 +87,8 @@
       </p>
     </BlockEx>
     <BlockEx header="Inkluze se týká většiny učitelů" gray>
-      S malou nadsázkou se dá se říci, že v českých školách nenajdete učitele, který by se v některé ze svých tříd nesetkal s problémy souvisejícími s inkluzí.
-      V tomto případě se mu jistě budou hodit znalosti z našich kurzů, jako například:
+      S malou nadsázkou se dá se říci, že v českých školách nenajdete učitele, který by se během své praxe nesetkal s problémy souvisejícími s inkluzí.
+      V tom případě se mu jistě budou hodit znalosti z našich kurzů, které řeší otázky typu:
       <ul className='fa-ul' style={{ marginTop: "8px" }}>
         <LiCheck>Existují nějaké speciální pomůcky nebo metody, které by mohly žákovi ve výuce pomoci?</LiCheck>
         <LiCheck>Kdo mu může poradit jak k žákovi přistupovat a řešit s ním problematické situace?</LiCheck>
@@ -100,32 +100,31 @@
         <LiCheck>Jak rozdělit pravomoc mezi učitele a asistenta a jak vůbec s asistentem spolupracovat?</LiCheck>
         <LiCheck>Jak efektivně komunikovat s rodiči žáka?
           Kdo s tím učiteli může pomoci?</LiCheck>
-        <LiCheck>A mnoho dalšího…</LiCheck>
+        <LiCheck>A mnoho dalších…</LiCheck>
       </ul>
     </BlockEx>
     <BlockEx header="Alespoň 24 hodin kurzu nebo nic">
       <p>
         V podmínkách pro využití dotace je stanoveno, že každý učitel, který se v rámci projektu účastní nějakého vzdělávacího kurzu, <b>musí v souhrnu absolvovat alespoň 24 hodin vzdělávání</b>.
-        Této podmínce se říká tzv.
-        {popups.bagatelni_podpora}.
+        Této podmínce se říká {popups.bagatelni_podpora}.
       </p>
       <p>
-        Připravili jsme proto kurzy v celkové délce 24 hodin resp.
+        Připravili jsme proto kurzy v celkové délce 24, resp.
         32 hodin.
-        Naše kurzy splňují všechny podmínky, aby na ně bylo možno použit peníze z dotace.
+        Naše kurzy splňují všechny podmínky tak, aby na ně bylo možno použit peníze z dotace.
       </p>
       <p>
-        Cílem kurzů je, aby učitelé získali základní orientaci v problematice inkluzivního vzdělávání.
-        U kurzu v délce 32 hodin zbude více prostoru na diskusi s učiteli, řešení konkrétních problémů, týkajících se dané školy, a praktické procvičování získaných znalostí.
+        Cílem kurzů je, aby se pokud možno celý pedagogický sbor dokázal orientovat v problematice inkluze.
+        U kurzu v délce 32 hodin zbude více prostoru na diskusi s učiteli, řešení konkrétních problémů týkajících se dané školy a praktické procvičování získaných znalostí.
       </p>
       <p>
         Více viz stránka {sitemapRouter.navigateTag<IKurzyPars>(inkluze.root.kurzy) }.
       </p>
     </BlockEx>
-    <BlockEx header="Další informace a služby, týkající se dotace na inkluzi" gray>
+    <BlockEx header="Další informace a služby týkající se dotace na inkluzi" gray>
       <p>
         Získat dotaci a využít ji není tak složité, jak se může při prvním čtení stovek stran oficiálních dokumentů zdát.
-        Rádi vám poskytneme další informace a služby, které vám zjednoduší čerpání dotace:
+        Rádi vám poskytneme další informace a služby, které vám čerpání dotace výrazně zjednoduší:
       </p>
       <h4>Seznámení s pravidly Výzvy</h4>
       <p>
@@ -136,7 +135,7 @@
         V případě nejasností rádi zodpovíme vaše otázky na stránce {sitemapRouter.navigateTag(inkluze.root.q_and_a) }.
       </p>
       <h4>Další služby</h4>
-      Pokud budete potřebovat, pomůžeme vám s vyplněním žádosti nebo s kompletním projektovým řízením (zasílání průběžných zpráv a závěrečné zprávy apod.), viz stránka {sitemapRouter.navigateTag(inkluze.root.dalsi) }.
+      Pokud budete potřebovat, pomůžeme vám s vyplněním žádosti nebo s kompletním projektovým řízením (zasílání průběžných zpráv, závěrečné zprávy apod.), viz stránka {sitemapRouter.navigateTag(inkluze.root.dalsi) }.
     </BlockEx>
   </Page>
 </div >;
