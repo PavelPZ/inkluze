e86d948231e6a4544e97930c44a6deece76560c2
Petr Janeček <pjanecek@langmaster.cz>
2021-01-05 09:01:50 +0100

    x

--- inkluze/apps/inkluze/pages/okurzy.tsx
+++ inkluze/apps/inkluze/pages/okurzy.tsx
@@ -58,6 +58,12 @@
           <p>Akreditováno pro ZŠ, SŠ, VOŠ a asistenty pedagoga: MSMT - 823/2019-1-77</p>
           <p>Cena: 1290 Kč/os, Lektor: PaedDr. Ellen Mlátilíková</p>
         </LiHand>
+        <LiHand><h4><b>4. 2. 2021 Role pedagoga v karierovém poradenství – 8 hodin</b></h4>
+          <h4><Label bsStyle="warning">kariérové vzdělávání</Label></h4>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=4.%202.%202021%20Role%20pedagoga%20v%20karierov%C3%A9m%20poradenstv%C3%AD%20%E2%80%93%208%20hodin&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Akreditováno pro ZŠ, SŠ, VOŠ: MSMT - 823/2019-1-77</p>
+          <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
+        </LiHand>
         <LiHand><h4><b>10. 2. 2021 Práce se žákem s problémovým chováním – 8 hodin</b></h4>
           <h4><Label bsStyle="warning">inkluze</Label></h4>
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=10.%202.%202021%20Pr%C3%A1ce%20se%20%C5%BE%C3%A1kem%20s%20probl%C3%A9mov%C3%BDm%20chov%C3%A1n%C3%ADm%20%E2%80%93%208%20hodin&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
@@ -67,7 +73,7 @@
         <LiHand><h4><b>17. 2. 2021 Práce s pedagogickým sborem-jak předejít syndromu vyhoření  (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">inkluze</Label></h4>
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=17.%202.%202021%20Pr%C3%A1ce%20s%20pedagogick%C3%BDm%20sborem-jak%20p%C5%99edej%C3%ADt%20syndromu%20vyho%C5%99en%C3%AD%20%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, ŠD a asistenty pedagoga: MSMT - 7071/2018-1-286</p>
+          <p>Akreditováno pro ZŠ, SŠ, VOŠ: MSMT - 7071/2018-1-286</p>
           <p>Cena: 1290 Kč/os, Lektor: PaedDr. Ellen Mlátilíková</p>
         </LiHand>
       </ul>
