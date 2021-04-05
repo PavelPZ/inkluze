b6db256b4b72de5297cfcb96e204cc208095f02c
Petr Janeček <pjanecek@langmaster.cz>
2021-04-05 12:08:26 +0200

    x

--- inkluze/apps/inkluze/pages/okurzy.tsx
+++ inkluze/apps/inkluze/pages/okurzy.tsx
@@ -148,9 +148,15 @@
           <p>Akreditováno pro ZŠ, ŠD, MŠ, ZUŠ: MSMT- 8265/2019-1-321</p>
           <p>Cena: 1490 Kč/os, Lektor: Mgr. Bc. Jan Šindelář</p>
         </LiHand>
-        <LiHand><h4><b>14. 6. 2021 Distanční výuka – formy a metody práce s žáky online? (8 hodin)</b></h4>
+        <LiHand><h4><b>3. 6. 2021 Rozvoj osobnosti pedagoga ve zvládání konfliktů (8 hodin)</b></h4>
+          <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=3.%206.%202021%20Rozvoj%20osobnosti%20pedagoga%20ve%20zvl%C3%A1d%C3%A1n%C3%AD%20konflikt%C5%AF%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Akreditováno pro ZŠ, ŠD, MŠ, ZUŠ, asistenti pedagoga: MSMT- 8265/2019-1-321</p>
+          <p>Cena: 1490 Kč/os, Lektor: Mgr. Jana Soukupová</p>
+        </LiHand>
+        <LiHand><h4><b>14. 6. 2021 Distanční výuka – formy a metody práce s žáky online (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">ICT</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=14.%206.%202021%20Distan%C4%8Dn%C3%AD%20v%C3%BDuka%20%E2%80%93%20formy%20a%20metody%20pr%C3%A1ce%20s%20%C5%BE%C3%A1ky%20online%3F%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=14.%206.%202021%20Distan%C4%8Dn%C3%AD%20v%C3%BDuka%20%E2%80%93%20formy%20a%20metody%20pr%C3%A1ce%20s%20%C5%BE%C3%A1ky%20online%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
           <p>Akreditováno pro ZŠ, SŠ, VOŠ, ZUŠ: MSMT- 21327/2020-2-533</p>
           <p>Cena: 1490 Kč/os, Lektor: Mgr. Bc. Jan Šindelář</p>
         </LiHand>
