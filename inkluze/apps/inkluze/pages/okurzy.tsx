70f7f830e9e986bbb0c170dc539abb6e498b48c3
Petr Janeček <pjanecek@langmaster.cz>
2021-06-02 10:53:12 +0200

    x

--- inkluze/apps/inkluze/pages/okurzy.tsx
+++ inkluze/apps/inkluze/pages/okurzy.tsx
@@ -10,42 +10,6 @@
     </BlockEx>
     <BlockEx>
       <ul className='fa-ul'>
-        <LiHand><h4><b>20. 5. 2021 Rozvoj osobnosti pedagoga ve zvládání konfliktů (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=20.%205.%202021%20Rozvoj%20osobnosti%20pedagoga%20ve%20zvl%C3%A1d%C3%A1n%C3%AD%20konflikt%C5%AF%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, ŠD, MŠ, ZUŠ, asistenti pedagoga: MSMT- 8265/2019-1-321</p>
-          <p>Cena: 1490 Kč/os, Lektor: Mgr. Jana Soukupová</p>
-        </LiHand>
-        <LiHand><h4><b>21. 5. 2021 Různé metody pro začlenění ICT technologií do výuky (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">ICT</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=21.%205.%202021%20R%C5%AFzn%C3%A9%20metody%20pro%20za%C4%8Dlen%C4%9Bn%C3%AD%20ICT%20technologi%C3%AD%20do%20v%C3%BDuky%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, SŠ, VOŠ, ZUŠ: MSMT- 8265/2019-1-321</p>
-          <p>Cena: 1490 Kč/os, Lektor: Mgr. Bc. Jan Šindelář</p>
-        </LiHand>
-        <LiHand><h4><b>26. 5. 2021 Rozvoj osobnosti pedagoga a zásady efektivní komunikace (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=26.%205.%202021%20Rozvoj%20osobnosti%20pedagoga%20a%20z%C3%A1sady%20efektivn%C3%AD%20komunikace%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, SŠ, VOŠ, ŠD, ŠK, DM: MSMT - 14332/2018-1-514</p>
-          <p>Cena: 1490 Kč/os, Lektor: PaedDr. Ellen Mlátilíková</p>
-        </LiHand>
-        <LiHand><h4><b>27. 5. 2021 Čtenářská a informační gramotnost na ZŠ – 8 hodin</b></h4>
-          <h4><Label bsStyle="warning">Čtenářská gramotnost</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=27.%205.%202021%20%C4%8Cten%C3%A1%C5%99sk%C3%A1%20a%20informa%C4%8Dn%C3%AD%20gramotnost%20na%20Z%C5%A0%20%E2%80%93%208%20hodin&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, asistenti pedagoga: MSMT- 7071/2018-1-286</p>
-          <p>Cena: 1490 Kč/os, Lektor: Mgr. Dana Forýtková</p>
-        </LiHand>
-        <LiHand><h4><b>28.5.2021 Základy práce s MS Office a jejich využití na školách (8 hod)</b></h4>
-          <h4><Label bsStyle="warning">ICT</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=28.5.2021%20Z%C3%A1klady%20pr%C3%A1ce%20s%20MS%20Office%20a%20jejich%20vyu%C5%BEit%C3%AD%20na%20%C5%A1kol%C3%A1ch%20(8%20hod)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, ŠD, MŠ, ZUŠ: MSMT- 8265/2019-1-321</p>
-          <p>Cena: 1490 Kč/os, Lektor: Mgr. Bc. Jan Šindelář</p>
-        </LiHand>
-        <LiHand><h4><b>1. 6. 2021 Čtenářská a informační gramotnost na ZŠ – 8 hodin</b></h4>
-          <h4><Label bsStyle="warning">Čtenářská gramotnost</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=1.%206.%202021%20%C4%8Cten%C3%A1%C5%99sk%C3%A1%20a%20informa%C4%8Dn%C3%AD%20gramotnost%20na%20Z%C5%A0%20%E2%80%93%208%20hodin&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, asistenti pedagoga: MSMT- 7071/2018-1-286</p>
-          <p>Cena: 1490 Kč/os, Lektor: Mgr. Dana Forýtková</p>
-        </LiHand>
         <LiHand><h4><b>3. 6. 2021 Rozvoj osobnosti pedagoga ve zvládání konfliktů (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=3.%206.%202021%20Rozvoj%20osobnosti%20pedagoga%20ve%20zvl%C3%A1d%C3%A1n%C3%AD%20konflikt%C5%AF%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
