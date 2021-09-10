01121b9cbd8128c494f524339cadde7c53528f4c
Petr Janeček <pjanecek@langmaster.cz>
2021-09-10 12:30:11 +0200

    Update okurzy.tsx

--- inkluze/apps/inkluze/pages/okurzy.tsx
+++ inkluze/apps/inkluze/pages/okurzy.tsx
@@ -1,8 +1,9 @@
 ﻿var Okurzy: React.StatelessComponent<{}> = (props: {}, ctx: sitemapRouter.IContext) => <div>
   <Page>
-    <BlockEx header="Nabízíme následující online kurzy uznatelné pro Šablony II">
+    <BlockEx header="Nabízíme následující prezenční kurzy uznatelné pro Šablony II">
       <p>
         Pokud se chcete na některý z kurzů přihlásit nebo máte ke kurzu nějaký upřesňující dotaz, kontaktujte nás prosím na emailu projekt@langmaster.cz nebo na telefonech 244 460 807, 728 234 285</p>
+      <p>Místo konání bude vždy upřesněno po dohodě s přihlášenými účastníky.</p>
       <p>
         Podle předběžného zájmu pedagogů budeme průběžně vypisovat další termíny. Kontaktujte nás, pokud sháníte nějaký kurz, který v seznamu nevidíte.</p>
     </BlockEx>
@@ -78,9 +79,9 @@
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Veronika Havelková</p>
           <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
         </LiHand>
-        <LiHand><h4><b>22. 11. 2021 Osobnostní a sociální rozvoj učitele ve zvládání konfliktů (8 hodin)</b></h4>
+        <LiHand><h4><b>24. 11. 2021 Osobnostní a sociální rozvoj učitele ve zvládání konfliktů (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=22.%2011.%202021%20Osobnostn%C3%AD%20a%20soci%C3%A1ln%C3%AD%20rozvoj%20u%C4%8Ditele%20ve%20zvl%C3%A1d%C3%A1n%C3%AD%20konflikt%C5%AF%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=24.%2011.%202021%20Osobnostn%C3%AD%20a%20soci%C3%A1ln%C3%AD%20rozvoj%20u%C4%8Ditele%20ve%20zvl%C3%A1d%C3%A1n%C3%AD%20konflikt%C5%AF%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
           <p>Akreditováno pro ZŠ, SŠ, VOŠ: MSMT- 32248/2020-2-752</p>
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
           <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
