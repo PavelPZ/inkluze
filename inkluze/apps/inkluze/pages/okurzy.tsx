7bed368c09e4e0f558a3f7701d987cb9a7a2cff5
Petr Janeček <pjanecek@langmaster.cz>
2021-10-08 09:29:19 +0200

    Update okurzy.tsx

--- inkluze/apps/inkluze/pages/okurzy.tsx
+++ inkluze/apps/inkluze/pages/okurzy.tsx
@@ -9,13 +9,7 @@
     </BlockEx>
     <BlockEx>
       <ul className='fa-ul'>
-        <LiHand><h4><b>7. 10. 2021 Jak zvládat stresové situace a nedospět k vyhoření (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=7.%2010.%202021%20Jak%20zvl%C3%A1dat%20stresov%C3%A9%20situace%20a%20nedosp%C4%9Bt%20k%20vyho%C5%99en%C3%AD%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro MŠ, ZŠ, ŠD, SŠ, VOŠ, DM, ZUŠ, asistenti: MSMT- 7694/2021-4-275</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Jana Soukupová</p>
-          <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
-        </LiHand>
+
         <LiHand><h4><b>12.10. 2021 Inspirace pro využití interaktivních tabulí ve škole (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">ICT</Label></h4>
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=12.10.%202021%20Inspirace%20pro%20vyu%C5%BEit%C3%AD%20interaktivn%C3%ADch%20tabul%C3%AD%20ve%20%C5%A1kole%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
@@ -23,26 +17,26 @@
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Bc. Jan Šindelář</p>
           <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
         </LiHand>
-        <LiHand><h4><b>21. 10. 2021 Zásady asertivní a efektivní komunikace pro MŠ, ZŠ, ZUŠ (8 hodin)</b></h4>
+        <LiHand><h4><b>21. 10. 2021 Jak zvládat stresové situace a nedospět k vyhoření (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=21.%2010.%202021%20Z%C3%A1sady%20asertivn%C3%AD%20a%20efektivn%C3%AD%20komunikace%20pro%20M%C5%A0%2C%20Z%C5%A0%2C%20ZU%C5%A0%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-        <p>Akreditováno pro MŠ, ZŠ, ŠD, ZUŠ, asistenti: MSMT- 8265/2019-1-321</p>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=21.%2010.%202021%20Jak%20zvl%C3%A1dat%20stresov%C3%A9%20situace%20a%20nedosp%C4%9Bt%20k%20vyho%C5%99en%C3%AD%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Akreditováno pro MŠ, ZŠ, ŠD, SŠ, VOŠ, DM, ZUŠ, asistenti: MSMT- 7694/2021-4-275</p>
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Jana Soukupová</p>
-          <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
+          <p>Místo školení: České Budějovice</p>
         </LiHand>
         <LiHand><h4><b>2.11. 2021 Práce s dotykovými zařízeními ve škole (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">ICT</Label></h4>
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=2.11.%202021%20Pr%C3%A1ce%20s%20dotykov%C3%BDmi%20za%C5%99%C3%ADzen%C3%ADmi%20ve%20%C5%A1kole%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
           <p>Akreditováno pro MŠ, ZŠ, ŠD, SŠ, VOŠ, ZUŠ, asistenti: MSMT- 13854/2021-2-500</p>
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Bc. Jan Šindelář</p>
-          <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
+          <p>Místo školení: Soběchleby (okres Přerov).</p>
         </LiHand>
-        <LiHand><h4><b>4.11. 2021 Podnikání, finanční gramotnost a manažerské dovednosti–8 hod</b></h4>
+        <LiHand><h4><b>4.11. 2021 Rozvoj podnikavosti a zásady soukromého podnikání – 8 hodin</b></h4>
           <h4><Label bsStyle="warning">Výchova k podnikavosti</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=4.11.%202021%20Rozvoj%20%C4%8Dten%C3%A1%C5%99sk%C3%A9%20gramotnosti%20u%20%C5%BE%C3%A1k%C5%AF%20Z%C5%A0%20%E2%80%93%208%20hodin&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro SŠ, VOŠ: MSMT- 823/2019-1-77</p>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=4.11.%202021%20Rozvoj%20podnikavosti%20a%20z%C3%A1sady%20soukrom%C3%A9ho%20podnik%C3%A1n%C3%AD%20%E2%80%93%208%20hodin&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Akreditováno pro SŠ, VOŠ: MSMT- 32853/2018-1-914</p>
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Jana Soukupová</p>
-          <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
+          <p>Místo školení: Rychnov nad Kněžnou</p>
         </LiHand>
         <LiHand><h4><b>4.11. 2021 Rozvoj čtenářské gramotnosti u žáků ZŠ – 8 hodin  </b></h4>
           <h4><Label bsStyle="warning">Čtenářská gramotnost</Label></h4>
@@ -63,7 +57,7 @@
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=11.%2011.%202021%20%C4%8Cten%C3%A1%C5%99sk%C3%A1%20gramotnost%20nap%C5%99%C3%AD%C4%8D%20v%C5%A1emi%20p%C5%99edm%C4%9Bty%20S%C5%A0%20a%20VO%C5%A0%20%20-%208%20hod&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
           <p>Akreditováno pro ZŠ, SŠ, VOŠ: MSMT- 11115/2020-2-287</p>
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
-          <p>Místo školení bude upřesněno po dohodě s přihlášenými účastníky.</p>
+          <p>Místo školení: Rychnov nad Kněžnou</p>
         </LiHand>
         <LiHand><h4><b>15. 11. 2021  Moderní technologie při rozvoji matematické gramotnosti na ZŠ (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">Matematická gramotnost</Label></h4>
