622a8404e159ef2a705fd2140944def120c79a2d
Petr Janeček <pjanecek@langmaster.cz>
2021-11-29 12:49:06 +0100

    Update okurzy.tsx

--- inkluze/apps/inkluze/pages/okurzy.tsx
+++ inkluze/apps/inkluze/pages/okurzy.tsx
@@ -14,34 +14,6 @@
     <BlockEx>
       <ul className='fa-ul'>
 
-        <LiHand><h4><b>18.11. 2021 Inspirace pro využití interaktivních tabulí ve škole (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">ICT</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=18.11.%202021%20Inspirace%20pro%20vyu%C5%BEit%C3%AD%20interaktivn%C3%ADch%20tabul%C3%AD%20ve%20%C5%A1kole%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro MŠ, ZŠ, ŠD, SŠ, VOŠ, ZUŠ, asistenti: MSMT- 13854/2021-2-500</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Bc. Jan Šindelář</p>
-          <p>Místo školení: online</p>
-        </LiHand>
-        <LiHand><h4><b>24. 11. 2021 Rozvoj osobnosti pedagoga v oblasti komunikace (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=24.%2011.%202021%20Rozvoj%20osobnosti%20pedagoga%20v%20oblasti%20komunikace%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro ZŠ, MŠ, ŠD, ZUŠ, asistenti: MSMT- 8265/2019-1-321</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
-          <p>Místo školení: online</p>
-        </LiHand>
-        <LiHand><h4><b>25.11. 2021 Jak zvládat stresové situace a nedospět k vyhoření (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">Osobnostní rozvoj</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=25.11.%202021%20Jak%20zvl%C3%A1dat%20stresov%C3%A9%20situace%20a%20nedosp%C4%9Bt%20k%20vyho%C5%99en%C3%AD%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro MŠ, ZŠ, ŠD, SŠ, VOŠ, ZUŠ, asistenti: MSMT- 7694/2021-4-275</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Jana Soukupová</p>
-          <p>Místo školení: online</p>
-        </LiHand>
-        <LiHand><h4><b>29.11. 2021 Využití ICT ve škole (8 hodin)</b></h4>
-          <h4><Label bsStyle="warning">ICT</Label></h4>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=29.11.%202021%20Vyu%C5%BEit%C3%AD%20ICT%20ve%20%C5%A1kole%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>Akreditováno pro MŠ, ZŠ, ŠD, SŠ, VOŠ, ZUŠ, asistenti: MSMT- 13854/2021-2-500</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Veronika Havelková</p>
-          <p>Místo školení: online</p>
-        </LiHand>
         <LiHand><h4><b>30.11. 2021 Jak na distanční výuku matematiky? (8 hodin)</b></h4>
           <h4><Label bsStyle="warning">Matematická gramotnost</Label></h4>
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=30.11.%202021%20Jak%20na%20distan%C4%8Dn%C3%AD%20v%C3%BDuku%20matematiky%3F%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
@@ -56,6 +28,20 @@
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
           <p>Místo školení: online</p>
         </LiHand>
+        <LiHand><h4><b>25.1. 2022 Efektivní využití MS Office pro pedagogy – 8 hodin</b></h4>
+          <h4><Label bsStyle="warning">ICT</Label></h4>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=25.1.%202022%20Efektivn%C3%AD%20vyu%C5%BEit%C3%AD%20MS%20Office%20pro%20pedagogy%20%E2%80%93%208%20hodin&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Akreditováno pro ZŠ, SŠ, VOŠ: MSMT- 13854/2021-2-500</p>
+          <p>Cena: 1590 Kč/os, Lektor: Mgr. Veronika Havelková</p>
+          <p>Místo školení: online</p>
+        </LiHand>
+        <LiHand><h4><b>26.1. 2022 GeoGebra - nástroj rozvoje matematické gramotnosti na SŠ (8 hodin)</b></h4>
+          <h4><Label bsStyle="warning">Matematická gramotnost</Label></h4>
+          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=26.1.%202022%20GeoGebra%20-%20n%C3%A1stroj%20rozvoje%20matematick%C3%A9%20gramotnosti%20na%20S%C5%A0%20(8%20hodin)&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
+          <p>Akreditováno pro SŠ a VOŠ: MSMT - 11115/2020-2-287</p>
+          <p>Cena: 1590 Kč/os, Lektor: Mgr. Veronika Havelková</p>
+          <p>Místo školení: online</p>
+        </LiHand>
         <LiHand><h4><b>27.1. 2022 Výchova k podnikavosti a manažerské dovednosti–8 hod</b></h4>
           <h4><Label bsStyle="warning">Výchova k podnikavosti</Label></h4>
           <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=27.1.%202022%20V%C3%BDchova%20k%20podnikavosti%20a%20mana%C5%BEersk%C3%A9%20dovednosti%E2%80%938%20hod&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A%20%0A%20%20%0APokud%20do%20jednoho%20pracovn%C3%ADho%20dne%20nedostanete%20potvrzen%C3%AD%20o%20p%C5%99ihl%C3%A1%C5%A1en%C3%AD%20na%20kurz%2C%20volejte%20pros%C3%ADm%20na%20%C4%8D%C3%ADslo%20244%20460%20807.">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
@@ -63,6 +49,7 @@
           <p>Cena: 1590 Kč/os, Lektor: Mgr. Jana Soukupová</p>
           <p>Místo školení: online</p>
         </LiHand>
+
       </ul>
     </BlockEx>
 
