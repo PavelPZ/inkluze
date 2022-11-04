f28b25be437fedabe0c9091d772f6a295e5573f3
Petr Janeček <pjanecek@langmaster.cz>
2022-11-04 11:04:44 +0100

    x

--- inkluze/apps/inkluze/pages/okurzy.tsx
+++ inkluze/apps/inkluze/pages/okurzy.tsx
@@ -14,55 +14,6 @@
     </BlockEx>
     <BlockEx>
       <ul className='fa-ul'>
-        <LiHand><h4><b>13.10. 2022  <a href="/index.html?home|kurzyss|SSNoveTechnologie"> Využití ICT ve škole</a></b></h4>
-          <h4><Label bsStyle="warning">Oblast: Vzdělání s využitím nových technologií</Label></h4>
-          <p>Akreditace: MSMT- 13854/2021-2-500</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Veronika Havelková</p>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=13.10.%202022%20Vyu%C5%BEit%C3%AD%20ICT%20ve%20%C5%A1kole&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>v rámci Šablon OP JAK se mohou účastnit všechny cílové skupiny</p>
-        </LiHand>
-        <LiHand><h4><b>14.10. 2022  <a href="/index.html?home|kurzyss|SSPedagogickaDiagnostika"> Sociální klima třídy a pedagogická diagnostika učitele</a></b></h4>
-          <h4><Label bsStyle="warning">Oblast: Pedagogická diagnostika</Label></h4>
-          <p>Akreditace: MSMT- 14644/2022-4-517</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=14.10.%202022%20Soci%C3%A1ln%C3%AD%20klima%20t%C5%99%C3%ADdy%20a%20pedagogick%C3%A1%20diagnostika%20u%C4%8Ditele&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>v rámci Šablon OP JAK se mohou účastnit všechny cílové skupiny</p>
-        </LiHand>
-        <LiHand><h4><b>19.10. 2022  <a href="/index.html?home|kurzyss|SSKarierovePoradenstvi"> Žák nadaný a mimořádně nadaný, možnosti jeho rozvoje-webinář</a></b></h4>
-          <h4><Label bsStyle="warning">Oblasti: Kariérové poradneství včetně identifikace a rozvoje nadání, Inkluze</Label></h4>
-          <p>Akreditace: MSMT- 14644/2022-4-517</p>
-          <p>Cena: 1590 Kč/os, Lektor: PaedDr. Ellen Mlátilíková</p>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=19.10.%202022%20%C5%BD%C3%A1k%20nadan%C3%BD%20a%20mimo%C5%99%C3%A1dn%C4%9B%20nadan%C3%BD%2C%20mo%C5%BEnosti%20jeho%20rozvoje-webin%C3%A1%C5%99&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>v rámci Šablon OP JAK se mohou účastnit všechny cílové skupiny</p>
-        </LiHand>
-        <LiHand><h4><b>25.10. 2022  <a href="/index.html?home|kurzyss|SSInkluzeCizinci"> Jak ve škole asertivně pracovat se žáky cizinci</a></b></h4>
-          <h4><Label bsStyle="warning">Oblast: Inkluze</Label></h4>
-          <p>Akreditace: MSMT- 14644/2022-4-517</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=25.10.%202022%20Jak%20ve%20%C5%A1kole%20asertivne%20pracovat%20se%20%C5%BE%C3%A1ky%20cizinci&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>v rámci Šablon OP JAK se mohou účastnit všechny cílové skupiny</p>
-        </LiHand>
-        <LiHand><h4><b>31.10. 2022 <a href="/index.html?home|kurzyss|SSVyukaCestiny"> Specifika výuky češtiny jako druhého jazyka</a></b></h4>
-          <h4><Label bsStyle="warning">Oblast: Výuka češtiny jako druhého jazyka</Label></h4>
-          <p>Akreditace: MSMT- 14644/2022-4-517</p>
-          <p>Cena: 1590 Kč/os, Lektor: PaedDr. Renée Grenarová, Ph.D</p>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=31.10.%202022%20Specifika%20v%C3%BDuky%20%C4%8De%C5%A1tiny%20jako%20druh%C3%A9ho%20jazyka&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>v rámci Šablon OP JAK se mohou účastnit všechny cílové skupiny</p>
-        </LiHand>
-        <LiHand><h4><b>2.11. 2022 <a href="/index.html?home|kurzyss|SSInovace"> Úpravy ŠVP v rámci programu InspIS ŠVP</a></b></h4>
-          <h4><Label bsStyle="warning">Oblast: Inovace ŠVP/koncepce rozvoje školy/školského zařízení</Label></h4>
-          <p>Akreditace: MSMT- 14644/2022-4-517</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Dana Forýtková</p>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=2.11.%202022%20%C3%9Apravy%20%C5%A0VP%20v%20r%C3%A1mci%20programu%20InspIS%20%C5%A0VP&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>tento termín bude speciálně zaměřen na ŠVP pro SŠ</p>
-        </LiHand>
-        <LiHand><h4><b>7.11. 2022  <a href="/index.html?home|kurzyss|SSMatematickaGramotnost"> GeoGebra - nástroj rozvoje matematické gramotnosti na SŠ</a></b></h4>
-          <h4><Label bsStyle="warning">Oblast: Matematická gramotnost</Label></h4>
-          <p>Akreditace: MSMT- 11115/2020-2-287</p>
-          <p>Cena: 1590 Kč/os, Lektor: Mgr. Veronika Havelková</p>
-          <p>Pro přihlášení <a href="mailto:projekt@langmaster.cz?subject=7.11.%202022%20GeoGebra%20-%20n%C3%A1stroj%20rozvoje%20matematick%C3%A9%20gramotnosti%20na%20S%C5%A0&body=Va%C5%A1e%20emailov%C3%A1%20adresa%3A%20%0AN%C3%A1zev%20%C5%A1koly%3A%20%0APo%C4%8Det%20p%C5%99ihl%C3%A1%C5%A1en%C3%BDch%20pedagog%C5%AF%3A%20%0APozn%C3%A1mka%3A">klikněte</a> nebo napište email na projekt@langmaster.cz</p>
-          <p>v rámci Šablon OP JAK se mohou účastnit všechny cílové skupiny</p>
-        </LiHand>
         <LiHand><h4><b>10.11. 2022  <a href="/index.html?home|kurzyss|SSFormativniHodnoceni"> Formativní hodnocení v praxi</a></b></h4>
           <h4><Label bsStyle="warning">Oblast: Formativní hodnocení</Label></h4>
           <p>Akreditace: MSMT- 14644/2022-4-517</p>
